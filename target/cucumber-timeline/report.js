e931ed4ea40b2e1e0f8c88c20a7e97979d09a5a9
VaraPrasad404 <54492157+VaraPrasad404@users.noreply.github.com>
2025-07-16 16:41:49 +0530

    commit

--- target/cucumber-timeline/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"ecd7ebab-4ce3-4f0c-8a0a-01b75255ac20","feature":"Fill Contact Us form","scenario":"Fill the contact us form in automation exercise website","start":1751456157223,"group":24,"content":"","tags":"@tag1,","end":1751456182567,"className":"failed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":24,"content":"Thread[#24,TestNG-tests-1,5,main]"}]);
-});
